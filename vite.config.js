71c953bf07cfd68596e969dab7966316e9f8402d
akshay121326 <akshay121326@gmail.com>
2026-02-04 13:08:46 +0530

    Add build-time diagnostics to vite config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+  console.log('--- VITE BUILD DEBUG ---');
+  console.log('Build Mode:', mode);
+  console.log('VITE_FIREBASE_API_KEY detected:', !!env.VITE_FIREBASE_API_KEY);
+  console.log('------------------------');
+
+  return {
+    plugins: [react()],
+  }
 })
